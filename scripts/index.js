d550d0ce48ddcb2703bd3098050f6b6860ba9af0
boymelvs <ambo_dacoycoy@yahoo.com>
2021-11-27 11:55:53 +0300

    updates

--- scripts/index.js
+++ scripts/index.js
@@ -12,26 +12,47 @@ const showBurger = (lines) => {
    });
 };
 
-/* show/hide arrow scroll up */
-window.addEventListener("scroll", () => {
-   let currentPosition = window.pageYOffset;
-   currentPosition > 1000 ? arrowUP.classList.add("active") : arrowUP.classList.remove("active");
-   console.log("this", window.pageYOffset);
-});
+/* get the location of page */
+const getPosition = () => {
+   let currentPosition = Math.floor(window.pageYOffset / 100) * 100;
 
-/* move up the page when arrow scrollUp click */
-const moveUp = () => {
-   const scrollStep = () => {
-      window.pageYOffset === 0 ? clearInterval(timer) : window.scroll(0, window.pageYOffset - 100);
-   };
-
-   let timer = setInterval(scrollStep, 25);
+   return currentPosition;
 };
 
+/* move up/down the page */
+const moveUpDown = (myPosition, currentPosition) => {
+   const scrollStep = () => {
+      if (myPosition > currentPosition) {
+         window.pageYOffset === myPosition ? clearInterval(timer) : window.scroll(0, getPosition() + 100);
+      } else {
+         window.pageYOffset === myPosition ? clearInterval(timer) : window.scroll(0, getPosition() - 100);
+      }
+   };
+
+   let timer = setInterval(scrollStep, 17);
+};
+
+/* call show burger & add/remove classess */
 const closeOpen = (elements, value, lines) => {
    if (elements.length > 1) {
       elements.forEach((element) => {
          element.addEventListener("click", () => {
+            if (element.innerText === "About") {
+               moveUpDown(500, getPosition());
+            }
+
+            if (element.innerText === "Services") {
+               moveUpDown(1300, getPosition());
+            }
+
+            if (element.innerText === "Projects") {
+               moveUpDown(1900, getPosition());
+            }
+
+            if (element.innerText === "Testimonials") {
+               moveUpDown(2500, getPosition());
+            }
+
             active(value);
             showBurger(lines);
          });
@@ -47,15 +68,24 @@ const closeOpen = (elements, value, lines) => {
 };
 
 /** get elements from DOM */
-const menus = document.querySelectorAll(".menus");
-const burger = document.querySelector(".burger_container");
-const lines = document.querySelectorAll(".burger");
-const navOpen = document.querySelector(".list");
-const arrowUP = document.querySelector(".scroll_up");
+const menus = document.querySelectorAll(".menus"); /* get all list of menu */
+const burger = document.querySelector(".burger_container"); /* get the burger */
+const lines = document.querySelectorAll(".burger"); /* get all line of burger */
+const navBar = document.querySelector(".list"); /* get menu container */
+const arrowUP = document.querySelector(".scroll_up"); /* get up_arrow */
 
-closeOpen(burger, navOpen, lines); /* toggle burger & show menu list */
-closeOpen(menus, navOpen, lines); /* close menu list when active link in menu was click */
+closeOpen(burger, navBar, lines); /* toggle burger & show menu list */
+closeOpen(menus, navBar, lines); /* close menu list when active link in menu was click */
 
+/* listening when to show up_arrow */
+window.addEventListener("scroll", () => {
+   let arrowPosition = window.pageYOffset;
+
+   arrowPosition > 1000 ? arrowUP.classList.add("active") : arrowUP.classList.remove("active");
+   console.log("this", arrowPosition);
+});
+
+/* listening when up_arrow was click */
 arrowUP.addEventListener("click", () => {
-   moveUp();
+   moveUpDown(0);
 });
